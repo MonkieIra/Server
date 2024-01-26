c1d0a286de1db0d26565222c041e1809cdfaa5a4
Ira <irisha_matrosova@inbox.ru>
2024-01-26 15:45:02 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { Schools, Heroes } = require('./models');
+const { sequelize } = require('./db');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.use(cors());
+
+app.get('/schools' , (req, res) => {
+    Schools.findAll().then((schools)=>{
+        res.json(schools);
+    })
+    
+
+})
+
+app.get('/heroes' , (req, res) => {
+    Heroes.findAll({include: Schools}).then((heroes)=>{
+        res.json(heroes);
+    })
+    
+
+})
+
+
+sequelize.sync({}).then(()=>{
+    app.listen(PORT, ()=>(
+        console.log("Сервер запущен. Порт:" , PORT)
+        ))
+})
---
+++ models.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('./db');
+
+console.log(sequelize);
+
+const Heroes = sequelize.define('heroes', {
+    name: DataTypes.STRING,
+    actor: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    school_id: DataTypes.INTEGER,
+    wand: DataTypes.STRING,
+    alive: DataTypes.BOOLEAN,
+    img: DataTypes.STRING
+})
+
+const Schools = sequelize.define('schools', {
+    name: DataTypes.STRING,
+})
+
+Heroes.belongsTo(Schools, {
+    foreignKey: 'school_id',
+    targetKey: 'id'
+})
+
+module.exports = {Heroes, Schools};
