21117fab20d6083df0d5835545c17bf789cb3d3b
Ira <irisha_matrosova@inbox.ru>
2024-01-30 16:28:13 +0300

    Ага

--- app.js
+++ app.js
@@ -27,6 +27,18 @@ app.get('/heroes' , (req, res) => {
 })
 
 
+app.get('/heroesfilterBySchool/:id' , (req, res) =>{
+    const id = req.params.id;
+
+    Heroes.findAll({
+        where: {school_id:id},
+        include: Schools
+    }).then((heroes) =>{
+        res.json(heroes);
+    })
+});
+
+
 sequelize.sync({}).then(()=>{
     app.listen(PORT, ()=>(
         console.log("Сервер запущен. Порт:" , PORT)
